7f79c8d3d69ca734ed85218a1e9be2274ba1a0f8
Orlando <substence@gmail.com>
2018-07-30 12:35:57 -0400

    cleanup before staring work on phase 2.

--- UnityFlappyBirdGameJam/Assets/Scripts/ObstacleSpawner.cs
+++ UnityFlappyBirdGameJam/Assets/Scripts/ObstacleSpawner.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Timers;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class ObstacleSpawner : MonoBehaviour
 {
